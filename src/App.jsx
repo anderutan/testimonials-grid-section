2c9ff1b9ba9ccb745176bc348a310bd1a13a9a58
anderu <andrewtan1310@gmail.com>
2024-04-11 15:58:55 +0800

    refactor to match desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Card from './components/Card';
+import data from './Data';
 
 function App() {
+  const cardStyles = [
+    'bg-violet-700 text-white col-start-1 col-end-3 row-start-1 row-end-2',
+    'bg-slate-600 text-white col-start-3 col-end-4 row-start-1 row-end-2',
+    'bg-white text-slate-600 col-start-1 col-end-2 row-start-2 row-end-3',
+    'bg-gray-800 text-white col-start-2 col-end-4 row-start-2 row-end-3',
+    'bg-white text-gray-800 col-start-4 col-end-5 row-start-1 row-end-3',
+  ];
   return (
-    <>
-      <Card />
-    </>
+    <div className='w-full h-full md:h-screen bg-slate-300 flex items-center justify-center'>
+      <div className='max-w-7xl md:grid md:grid-cols-4 md:grid-rows-2'>
+        <Card review={data[0]} index={0} cardStyles={cardStyles[0]} />
+        <Card review={data[1]} index={1} cardStyles={cardStyles[1]} />
+        <Card review={data[2]} index={2} cardStyles={cardStyles[2]} />
+        <Card review={data[3]} index={3} cardStyles={cardStyles[3]} />
+        <Card review={data[4]} index={4} cardStyles={cardStyles[4]} />
+      </div>
+    </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,49 +1,36 @@
-import data from '../Data';
-
-export default function Card() {
-  const cardStyles = [
-    'bg-violet-700 text-white',
-    'bg-slate-600 text-white',
-    'bg-white text-slate-600',
-    'bg-gray-800 text-white',
-    'bg-white text-gray-800',
-  ];
-  return data.map((review, index) => (
+export default function Card({ review, index, cardStyles }) {
+  const { image, name, verified, title, comment, icon } = review;
+  return (
     <div
       key={index}
-      className={`${
-        cardStyles[index % cardStyles.length]
-      } px-9 py-8 m-6 rounded-xl relative`}
+      className={`${cardStyles} px-9 py-8 md:px-6 md:py-6 m-6 md:m-3 rounded-xl relative shadow-lg`}
     >
       <div className='flex flex-col gap-5 z-10 relative'>
         <div className='flex gap-5'>
           <div className='w-10'>
             <img
-              src={review.image}
+              src={image}
               alt=''
               className='rounded-full ring-2 ring-current'
             />
           </div>
           <div>
-            <p className='font-bold mb-[-2px]'>{review.name}</p>
+            <p className='font-bold mb-[-2px]'>{name}</p>
             <p className='text-sm font-medium opacity-50'>
-              {review.verified ? 'Verified Graduate' : ''}
+              {verified ? 'Verified Graduate' : ''}
             </p>
           </div>
         </div>
         <div>
-          <p className='text-2xl leading-7 font-semibold'>{review.title}</p>
+          <p className='text-2xl leading-7 font-semibold'>{title}</p>
         </div>
         <div>
-          <p className='opacity-70 pb-3'>"{review.comment}"</p>
+          <p className='opacity-70'>"{comment}"</p>
         </div>
       </div>
-      {review.icon && (
-        <img
-          src={review.icon}
-          className='absolute right-10 top-0 z-0 opacity-80'
-        />
+      {icon && (
+        <img src={icon} className='absolute right-10 top-0 z-0 opacity-80' />
       )}
     </div>
-  ));
+  );
 }
